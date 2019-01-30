16589de128d1c1ef0ed9bd8b7a9010559fdefa08
ElenAlmeida <elenalmeida@outlook.com>
2019-01-30 17:50:55 -0200

    Inserção do test.js, Readme e imagem.

--- src/cipher.js
+++ src/cipher.js
@@ -1,44 +1,53 @@
-function cipherencode(){ 
-  const variavel= document.getElementById('string').value;
-  const set=document.getElementById('offset').value; 
-  const setup= parseInt(set);
-  console.log('ver', setup)
+function cipherencode(variavel, setup){ 
   let cod="";
   let codAsc=[];
   for(let i=0; i<variavel.length; i++){
         codAsc=variavel[i].charCodeAt();
         console.log('result', codAsc)
     if(codAsc>=65 && codAsc<=90){
-      cod+=String.fromCharCode(((codAsc-65+setup)%26)+65);
+      cod+=String.fromCharCode(((codAsc-65+(setup %26)+26)%26)+65);
     }
     else if(codAsc>=97 && codAsc<=122){
-      cod+=String.fromCharCode(((codAsc-97+setup)%26)+97);
+      cod+=String.fromCharCode(((codAsc-97+(setup %26)+26)%26)+97);
       console.log('ver', cod)
     }else{
       cod+=String.fromCharCode(codAsc);
     }
   }
-  document.getElementById('msgcodificada').innerHTML=(cod);
+ return cod;
 }
 
-function cipherdecode(){ 
-  const variavel2= document.getElementById('string').value;
-  const set2=document.getElementById('offset').value;
-  const setup2=parseInt(set2);
-  console.log('ver', setup2)
+function cipherdecode(variavel2, setup2){ 
   let cod2="";
   let codAsc2=[];
   for(let i=0; i<variavel2.length; i++){
         codAsc2=variavel2[i].charCodeAt();
-        console.log('result', codAsc2)
+        console.log('result',codAsc2)
     if(codAsc2>=65 && codAsc2<=90){
-      cod2+= String.fromCharCode(((codAsc2-65-(setup2 %26)+26)%26)+65);
+      cod2+=String.fromCharCode(((codAsc2-65-(setup2 %26)+26)%26)+65);
     }
     else if(codAsc2>=97 && codAsc2<=122){
-      cod2+= String.fromCharCode(((codAsc2-97-(setup2 %26)+26)%26)+97);
+      cod2+=String.fromCharCode(((codAsc2-97-(setup2 %26)+26)%26)+97);
+      console.log('ver',cod2)
     }else{
       cod2+=String.fromCharCode(codAsc2);
     }
   }
-  document.getElementById("msgdescod").innerHTML=(cod2);
+ return cod2;
+}
+
+function cipher(){
+  let variavel= document.getElementById('string').value;
+ console.log('ver', variavel)
+  let stu=document.getElementById('offset').value; 
+  let setup= parseInt(stu);
+  console.log('ver', setup);
+  document.getElementById('msgcodificada').innerHTML=cipherencode(variavel, setup);
+}
+function cipher2(){
+  const variavel2= document.getElementById('string').value;
+  const sut=document.getElementById('offset').value; 
+  const setup2= parseInt(sut);
+  console.log('ver', setup2)
+  document.getElementById('msgdescod').innerHTML=cipherdecode(variavel2, setup2);
 }
--- src/tests.js
+++ src/tests.js
@@ -1,39 +1,44 @@
-function cipherencode(){ 
-  const variavel= document.getElementById('string').value;
-  const set=document.getElementById('offset').value; 
-  const setup= parseInt(set);
-  console.log('ver', setup)
-  let cod="";
-  let codAsc=[];
-  for(let i=0; i<variavel.length; i++){
-        codAsc=variavel[i].charCodeAt();
-        console.log('result', codAsc)
-    if(codAsc>=65 && codAsc<=90){
-      cod+=String.fromCharCode(((codAsc-65+setup)%26)+65);
-    }
-    if(codAsc>=97 && codAsc<=122){
-      cod+=String.fromCharCode(((codAsc-97+setup)%26)+97);
-      console.log('ver', cod)
-    }
-  }
-  document.getElementById('msgcodificada').innerHTML=(cod);
+
+var resposta= cipherencode('abcdefghijklmnopqrstuvxz',1);
+if(resposta=='bcdefghijklmnopqrstuvwya'){
+  console.log('Se deu bem');
+}else{
+  console.log('Se deu mal, resposta correta bcdefghijklmnopqrstuvwya');
+}
+var resposta1= cipherdecode('bcdefghijklmnopqrstuvwya',1);
+if(resposta1=='abcdefghijklmnopqrstuvxz'){
+  console.log('Positive');
+}else{
+  console.log('Error')
 }
 
-function cipherdecode(){ 
-  const variavel2= document.getElementById('string').value;
-  const set2=document.getElementById('offset').value;
-  const setup2=parseInt(set2);
-  let cod2="";
-  let codAsc2=[];
-  for(let i=0; i<variavel2.length; i++){
-        codAsc2=variavel2[i].charCodeAt();
-        console.log('result', codAsc2)
-    if(codAsc2>=65 && codAsc2<=90){
-      cod2+= String.fromCharCode(((codAsc2-65-setup2)%26)+65);
-    }
-    if(codAsc2>=97 && codAsc2<=122){
-      cod2+= String.fromCharCode(((codAsc2-97-setup2)%26)+97);
-    }
-  }
-  document.getElementById("msgdescod").innerHTML=(cod2);
+var resposta02=cipherencode('ABCabc wxyzWXYZ @#$%12345678', 48);
+if(resposta02=='WXYwxy stuvSTUV @#$%12345678'){
+  console.log('Positivo')
+}else{
+  console.log('Error caracter')
 }
+
+var resposta03=cipherdecode('WXYwxy stuvSTUV @#$%12345678', 48);
+if(resposta03=='ABCabc wxyzWXYZ @#$%12345678'){
+  console.log("Positivo")
+}else{
+  console.log('Error caracter')
+}
+
+var resposta04=cipherdecode('PQRpqr lmnoLMNO @#$%12345678', -63);
+if(resposta04=='ABCabc wxyzWXYZ @#$%12345678'){
+  console.log("Positivo")
+}else{
+  console.log('Error caracter')
+}
+
+var resposta05=cipherencode('ABCabc wxyzWXYZ @#$%12345678', -63);
+if(resposta05=='PQRpqr lmnoLMNO @#$%12345678'){
+  console.log('Positivo')
+}else{
+  console.log('Error caracter')
+}
+
+
+
