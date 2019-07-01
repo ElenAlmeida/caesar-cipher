e1a27d8865a6335616f2ac629bb62301a0d76726
ElenAlmeida <elenalmeida@outlook.com>
2019-07-01 07:20:49 -0300

    Correcao de nome de variaveis

--- src/tests.js
+++ src/tests.js
@@ -1,39 +1,39 @@
 
-var resposta= cipherencode('abcdefghijklmnopqrstuvxz',1);
+const resposta= cipherEncode('abcdefghijklmnopqrstuvxz',1);
 if(resposta==='bcdefghijklmnopqrstuvwya'){
   console.log('Se deu bem')
 }else{
   console.log('Se deu mal, resposta correta bcdefghijklmnopqrstuvwya')
 }
-var resposta1= cipherdecode('bcdefghijklmnopqrstuvwya',1);
+const resposta1= cipherDecode('bcdefghijklmnopqrstuvwya',1);
 if(resposta1==='abcdefghijklmnopqrstuvxz'){
   console.log('Positive')
 }else{
   console.log('Error')
 }
 
-var resposta02=cipherencode('ABCabc wxyzWXYZ @#$%12345678', 48);
+const resposta02=cipherEncode('ABCabc wxyzWXYZ @#$%12345678', 48);
 if(resposta02==='WXYwxy stuvSTUV @#$%12345678'){
   console.log('Positivo')
 }else{
   console.log('Error caracter')
 }
 
-var resposta03=cipherdecode('WXYwxy stuvSTUV @#$%12345678', 48);
+const resposta03=cipherDecode('WXYwxy stuvSTUV @#$%12345678', 48);
 if(resposta03==='ABCabc wxyzWXYZ @#$%12345678'){
   console.log("Positivo")
 }else{
   console.log('Error caracter')
 }
 
-var resposta04=cipherdecode('PQRpqr lmnoLMNO @#$%12345678', -63);
+const resposta04=cipherDecode('PQRpqr lmnoLMNO @#$%12345678', -63);
 if(resposta04==='ABCabc wxyzWXYZ @#$%12345678'){
   console.log("Positivo")
 }else{
   console.log('Error caracter')
 }
 
-var resposta05=cipherencode('ABCabc wxyzWXYZ @#$%12345678', -63);
+const resposta05=cipherEncode('ABCabc wxyzWXYZ @#$%12345678', -63);
 if(resposta05==='PQRpqr lmnoLMNO @#$%12345678'){
   console.log('Positivo')
 }else{
