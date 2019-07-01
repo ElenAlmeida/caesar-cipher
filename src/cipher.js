b75ea53d48bc5e80cc1a4603954c3de06f3c0c87
ElenAlmeida <elenalmeida@outlook.com>
2019-07-01 06:55:12 -0300

    Refatorei as funções cipher encode e decode, alterei nomes de que estavam em português.

--- src/cipher.js
+++ src/cipher.js
@@ -1,46 +1,55 @@
-function cipherencode(messenger, move){ 
-  let cod="";
-  let codAsc=[];
-  for(let i=0; i<messenger.length; i++){
-        codAsc=messenger[i].charCodeAt();        
-    if(codAsc>=65 && codAsc<=90){
-      cod+=String.fromCharCode(((codAsc-65+(move %26)+26)%26)+65);
-    }
-    else if(codAsc>=97 && codAsc<=122){
-      cod+=String.fromCharCode(((codAsc-97+(move %26)+26)%26)+97);      
-    }else{
-      cod+=String.fromCharCode(codAsc);
-    }
-  }
- return cod;
-}
+const encode = document.getElementById('encode');
+encode.addEventListener('click', cipherCode);
 
-function cipherdecode(messenger2, move2){ 
-  let cod2="";
-  let codAsc2=[];
-  for(let i=0; i<messenger2.length; i++){
-      codAsc2=messenger2[i].charCodeAt();             
-    if(codAsc2>=65 && codAsc2<=90){
-      cod2+=String.fromCharCode(((codAsc2-65-(move2 %26)+26)%26)+65);
-    }
-    else if(codAsc2>=97 && codAsc2<=122){
-      cod2+=String.fromCharCode(((codAsc2-97-(move2 %26)+26)%26)+97);      
-    }else{
-      cod2+=String.fromCharCode(codAsc2);
-    }
-  }
- return cod2;
-}
+const decode = document.getElementById('decode');
+decode.addEventListener('click', cipherDecrypted);
 
-function cipher(){
-  let messenger= document.getElementById('string').value;   
+function cipherEncode(message, displacement){
+  let result='';
+	let msgs= message.split('');
+
+  let turnsIntoCode = msgs.map(elem => elem.charCodeAt());
+  
+	turnsIntoCode.map(elem =>{
+		if(elem>=65 && elem<=90){
+			result+= String.fromCharCode(((elem-65+(displacement %26)+26)%26)+65);
+		}else if( elem>=97 && elem<=122){
+			result+= String.fromCharCode(((elem-97+(displacement %26)+26)%26)+97);
+		}else{
+			result+= String.fromCharCode(elem);
+		}
+  });
+  return result;		
+};
+
+function cipherDecode(message, displacement){
+  let result='';
+	let msgs= message.split('');
+
+  let turnsIntoCode = msgs.map(elem => elem.charCodeAt());
+  
+	turnsIntoCode.map(elem =>{
+		if(elem>=65 && elem<=90){
+			result+= String.fromCharCode(((elem-65-(displacement %26)+26)%26)+65);
+		}else if( elem>=97 && elem<=122){
+			result+= String.fromCharCode(((elem-97-(displacement %26)+26)%26)+97);
+		}else{
+			result+= String.fromCharCode(elem);
+		}
+  });
+  return result;		
+};
+
+function cipherCode(){
+  let message= document.getElementById('string').value;   
   let move= parseInt(document.getElementById('offset').value);
    
-  document.getElementById('msgcodificada').innerHTML=cipherencode(messenger, move);
+  document.getElementById('codedMessage').innerHTML=cipherEncode(message, move);
 }
-function cipher2(){
-  let messenger2= document.getElementById('string').value;
-  let move2= parseInt(document.getElementById('offset').value);
+
+function cipherDecrypted(){
+  let message= document.getElementById('string').value;
+  let move= parseInt(document.getElementById('offset').value);
    
-  document.getElementById('msgdescod').innerHTML=cipherdecode(messenger2, move2);
+  document.getElementById('decryptedMessage').innerHTML=cipherDecode(message, move);
 }
